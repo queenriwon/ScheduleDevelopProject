bb6d5625316c3086925ff3ba9ae6910dd9224396
queenriwon <queenriwon3@gmail.com>
2025-02-07 11:37:17 +0900

    feat: lv.1,2 - entity 작성

--- src/main/java/com/example/scheduledevelopproject/entity/Schedules.java
+++ src/main/java/com/example/scheduledevelopproject/entity/Schedules.java
@@ -1,4 +1,33 @@
 package com.example.scheduledevelopproject.entity;
 
-public class Schedules {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "schedules")
+@NoArgsConstructor
+public class Schedules extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Column(nullable = false)
+    private String todoTitle;
+
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String todoContents;
+
+    public Schedules(String todoTitle, String todoContents) {
+        this.todoTitle = todoTitle;
+        this.todoContents = todoContents;
+    }
 }
--- src/main/java/com/example/scheduledevelopproject/entity/Users.java
+++ src/main/java/com/example/scheduledevelopproject/entity/Users.java
@@ -1,5 +1,32 @@
 package com.example.scheduledevelopproject.entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class Users {
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class Users extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Users(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }

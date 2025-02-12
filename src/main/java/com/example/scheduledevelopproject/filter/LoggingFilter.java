664f006b8c8a4523336d0054e94e55f105b4ec18
queenriwon <queenriwon3@gmail.com>
2025-02-13 00:52:21 +0900

    style: 사용하지 않는 주석 및 메서드 삭제

--- src/main/java/com/example/scheduledevelopproject/filter/LoggingFilter.java
+++ src/main/java/com/example/scheduledevelopproject/filter/LoggingFilter.java
@@ -3,14 +3,12 @@ package com.example.scheduledevelopproject.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+@Slf4j
 public class LoggingFilter implements Filter {
-
-    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class.getName());
 
     @Override
     public void doFilter(
@@ -23,10 +21,10 @@ public class LoggingFilter implements Filter {
 
         String method = httpRequest.getMethod();
         String requestURI = httpRequest. getRequestURI();
-        logger.info("[REQUEST] {}, {}", method, requestURI);
+        log.info("[REQUEST] {}, {}", method, requestURI);
 
         filterChain.doFilter(servletRequest, servletResponse);
         int status = httpResponse.getStatus();
-        logger.info("[RESPONSE] 응답 완료: {}, {}, {}", method, requestURI, status);
+        log.info("[RESPONSE] 응답 완료: {}, {}, {}", method, requestURI, status);
     }
 }
